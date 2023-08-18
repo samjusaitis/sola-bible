f9eec5e96c05aa8c1e3a329d9b8e1dd017a89785
Sam Jusaitis <hello@samjusaitis.com>
2023-08-18 21:43:36 +0930

    fix eastons type

--- helpers/eastons.ts
+++ helpers/eastons.ts
@@ -1,13 +1,16 @@
 import { books } from '../lib/books';
+import { BookId } from '../types';
 
 /**
  * Returns the book id for the provided Easton's abbreviation.
  */
 export function eastonsAbbreviationToBookId(
   eastonsAbbr: string,
-): number | undefined {
-  // NOTE: an Easton's abbreviation is the same as the `nameAbbr` value
-  // in the books data, without any spaces.
+): BookId | undefined {
+  /**
+   * NOTE: an Easton's abbreviation is the same as the `nameAbbr` value
+   * in the books data, without any spaces.
+   */
   const book = books.find(
     (book) => book.nameAbbr.replace(/\s/g, '') === eastonsAbbr,
   );
